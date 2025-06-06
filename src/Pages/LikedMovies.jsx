a8fa7941012b9a8dc0966b986771497fd1dd376f
CrissP29 <pincaycriss29@gmail.com>
2025-06-05 22:31:28 -0500

    actualizacion de paquetes

--- src/Pages/LikedMovies.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import UserMovieSection from "../componets/UserMovieSection/UserMovieSection";
-
-function LikedMovies() {
-  return <UserMovieSection from="LikedMovies"></UserMovieSection>;
-}
-
-export default LikedMovies;
